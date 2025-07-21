465c9d56bd33e5526541c640b0597831eafdac75
Codyman-Mr <willbrightuyole@gmail.com>
2025-07-21 13:37:24 -0700

    Update main-local.php

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -3,41 +3,17 @@
 return [
     'components' => [
         'db' => [
-            'class' => \yii\db\Connection::class,
-            'dsn' => 'mysql:host=localhost;dbname=votingonline',
-            'username' => 'root',
-            'password' => '',
+            'class' => 'yii\db\Connection',
+            'dsn' => getenv('DB_DSN') ?: 'pgsql:host=dpg-d1v7hcjuibrs73958tug-a.oregon-postgres.render.com;port=5432;dbname=votingonline',
+            'username' => getenv('DB_USERNAME') ?: 'votingonline_user',
+            'password' => getenv('DB_PASSWORD') ?: 'fGjXQAlVJ8HenncVjDIKQex1Zpr3Q0YF',
             'charset' => 'utf8',
         ],
         'mailer' => [
             'class' => \yii\symfonymailer\Mailer::class,
             'viewPath' => '@common/mail',
-            // send all mails to a file by default.
             'useFileTransport' => true,
-            // You have to set
-            //
-            // 'useFileTransport' => false,
-            //
-            // and configure a transport for the mailer to send real emails.
-            //
-            // SMTP server example:
-            //    'transport' => [
-            //        'scheme' => 'smtps',
-            //        'host' => '',
-            //        'username' => '',
-            //        'password' => '',
-            //        'port' => 465,
-            //        'dsn' => 'native://default',
-            //    ],
-            //
-            // DSN example:
-            //    'transport' => [
-            //        'dsn' => 'smtp://user:pass@smtp.example.com:25',
-            //    ],
-            //
-            // See: https://symfony.com/doc/current/mailer.html#using-built-in-transports
-            // Or if you use a 3rd party service, see:
-            // https://symfony.com/doc/current/mailer.html#using-a-3rd-party-transport
+            // Usitumie transport kwa sasa kama hutumi barua pepe halisi
         ],
     ],
 ];
