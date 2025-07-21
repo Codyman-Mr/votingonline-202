000d3ac76300704ff903141ec8e58fd3d3c412f3
Codyman-Mr <willbrightuyole@gmail.com>
2025-07-21 13:27:19 -0700

    Update main-local.php

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -9,37 +9,12 @@ return [
             'password' => getenv('DB_PASSWORD') ?: 'fGjXQAlVJ8HenncVjDIKQex1Zpr3Q0YF',
             'charset' => 'utf8',
         ],
-    ],
-];
-    // ... mailer or other components ...
 
         'mailer' => [
             'class' => \yii\symfonymailer\Mailer::class,
             'viewPath' => '@common/mail',
-            // send all mails to a file by default.
             'useFileTransport' => true,
-            // You have to set
-            //
-            // 'useFileTransport' => false,
-            //
-            // and configure a transport for the mailer to send real emails.
-            //
-            // SMTP server example:
-            //    'transport' => [
-            //        'scheme' => 'smtps',
-            //        'host' => '',
-            //        'username' => '',
-            //        'password' => '',
-            //        'port' => 465,
-            //        'dsn' => 'native://default',
-            //    ],
-            //
-            // DSN example:
-            //    'transport' => [
-            //        'dsn' => 'smtp://user:pass@smtp.example.com:25',
-            //    ],
-            //
-            // See: https://symfony.com/doc/current/mailer.html#using-built-in-transports
-            // Or if you use a 3rd party service, see:
-            // https://symfony.com/doc/current/mailer.html#using-a-3rd-party-transport
-
+            // Unaweza ongeza 'transport' hapa kama unahitaji kutuma barua pepe halisi
+        ],
+    ],
+];
