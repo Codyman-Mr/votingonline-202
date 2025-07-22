6912ff0202f6ae012cbe11bc4c862348e5ddd8a5
Codyman-Mr <willbrightuyole@gmail.com>
2025-07-22 09:56:43 -0700

    Update SiteController.php

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -353,7 +353,7 @@ public function actionDetails()
             return $this->redirect(['voting/vote']);
         } else {
             // If voter is not found, show error message
-            $message = "Sorry, your details are incorrect.";
+            $message = "Ensure that you enter the correct information as you registered.";
         }
     }
 
