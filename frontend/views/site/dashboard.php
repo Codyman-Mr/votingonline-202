c349c9815c8891901b9821fb981a346c8e18e891
Codyman-Mr <willbrightuyole@gmail.com>
2025-07-22 02:03:15 -0700

    Update dashboard.php

--- frontend/views/site/dashboard.php
+++ frontend/views/site/dashboard.php
@@ -41,7 +41,6 @@ if (Yii::$app->user->isGuest) {
         <a href="<?= Url::to(['site/dashboard', 'section' => 'profile']) ?>">👤 Profile</a>
         <a href="<?= Url::to(['voting/register','section' =>'Register']) ?>">📝Register Now</a>
         <a href="<?= Url::to(['site/details']) ?>" data-method="post">🗳️Cast Vote</a>
- <a href="<?= Url::to(['candidate/index', 'section' => 'manage-elections']) ?>">👤 Manage Elections</a>
         <a href="<?= Url::to(['site/dashboard', 'section' => 'results']) ?>">📊 Results</a>
         <a href="<?= Url::to(['site/logout']) ?>" data-method="post">🚪 Logout</a>
         
