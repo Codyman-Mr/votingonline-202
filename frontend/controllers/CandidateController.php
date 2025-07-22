321518087ec1c28fec33fef245f4a8b30e3712e6
Codyman-Mr <willbrightuyole@gmail.com>
2025-07-22 00:54:26 -0700

    Update CandidateController.php

--- frontend/controllers/CandidateController.php
+++ frontend/controllers/CandidateController.php
@@ -10,5 +10,11 @@ use yii\web\UploadedFile;
 
 class CandidateController extends Controller
 {
-    // actions zako hapa
+    public function actionIndex()
+    {
+        $candidates = Candidate::find()->all();
+        return $this->render('index', ['candidates' => $candidates]);
+    }
+
+    // ... zingine action zako
 }
